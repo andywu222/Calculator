2b595b4c4acfe3e12eafdbcf25239c52eff45ce8
andywu222 <andywu2@cpp.edu>
2025-03-11 19:28:55 -0700

    Added subtract overflow and subtract underflow

--- test_calculator.c
+++ test_calculator.c
@@ -53,11 +53,25 @@ void test_subtract_zero(void) {
     TEST_ASSERT_EQUAL(10, subtract(10, 0)); 
     TEST_ASSERT_EQUAL(0, subtract(0, 0));
 }
+
+void test_subtract_overflow(void) {
+    int result = subtract(INT_MAX, 1); 
+    TEST_ASSERT_TRUE(result > 0); 
+}
+
+
+void test_subtract_underflow(void) {
+    int result = subtract(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result < 0); 
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_subtract_positive_numbers);
     RUN_TEST(test_subtract_positive_and_negative_numbers);
     RUN_TEST(test_subtract_negative_numbers);
     RUN_TEST(test_subtract_zero);
+    RUN_TEST(test_subtract_overflow);
+    RUN_TEST(test_subtract_underflow);
     return UNITY_END();
 }
