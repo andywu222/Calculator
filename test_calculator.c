f2329e77514d87b58c37ea9a4091f0d77a5a5333
andywu222 <andywu2@cpp.edu>
2025-03-11 19:37:42 -0700

    Added multiply overflow and multiply underflow

--- test_calculator.c
+++ test_calculator.c
@@ -81,11 +81,24 @@ void test_multiply_zero(void) {
     TEST_ASSERT_EQUAL(0, multiply(10, 0)); 
     TEST_ASSERT_EQUAL(0, multiply(0, 0));
 }
+
+void test_multiply_overflow(void) {
+    int result = subtract(INT_MAX, 1); 
+    TEST_ASSERT_TRUE(result > 0); 
+}
+
+void test_multiply_underflow(void) {
+    int result = multiply(INT_MAX, -1); 
+    TEST_ASSERT_TRUE(result < 0); 
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_multiply_positive_numbers);
     RUN_TEST(test_multiply_positive_and_negative_numbers);
     RUN_TEST(test_multiply_negative_numbers);
     RUN_TEST(test_multiply_zero);
+    RUN_TEST(test_multiply_overflow);
+    RUN_TEST(test_multiply_underflow);
     return UNITY_END();
 }
